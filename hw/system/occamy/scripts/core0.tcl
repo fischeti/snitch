be7e29660c1ef523662b02cce7e04cd54b571f59
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-06-29 14:17:30 +0200

    clean-up

--- hw/system/occamy/scripts/core0.tcl
+++ hw/system/occamy/scripts/core0.tcl
@@ -1,4 +1,5 @@
-set snitch_cc_0 /tb_bin/i_dut/i_occamy/i_occamy_quadrant_s1_0/i_occamy_cluster_0/i_cluster/gen_core[0]/i_snitch_cc
+set cluster0 /tb_bin/i_dut/i_occamy/i_occamy_quadrant_s1_0/i_occamy_cluster_0/i_cluster
+set snitch_cc_0 $cluster0/gen_core[0]/i_snitch_cc
 set cpu0 $snitch_cc_0/i_snitch
 set fpu0 $snitch_cc_0/gen_fpu/i_snitch_fp_ss
 set ssr0 $snitch_cc_0/gen_ssrs/i_snitch_ssr_streamer/gen_ssrs[0]/i_ssr
@@ -8,11 +9,21 @@ add wave -noupdate -expand -group {All Cores} $cpu0/wfi_q
 add wave -noupdate -expand -group {All Cores} $cpu0/pc_q
 add wave -noupdate -expand -group {All Cores} $cpu0/instret_q
 
+add wave -noupdate -divider Regfile
+add wave -noupdate -expand -group {REGFILE} $cpu0/gpr_raddr
+add wave -noupdate -expand -group {REGFILE} $cpu0/gpr_rdata
+add wave -noupdate -expand -group {REGFILE} $cpu0/gpr_waddr
+add wave -noupdate -expand -group {REGFILE} $cpu0/gpr_wdata
+add wave -noupdate -expand -group {REGFILE} $cpu0/gpr_we
+
+
 add wave -noupdate -divider FPUs
 add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/clk_i
 add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/rst_ni
 add wave -noupdate -expand -group FPU0 $fpu0/trace_port_o
 add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/op_i
+add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/in_valid_i
+add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/in_ready_o
 add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/out_valid_o
 add wave -noupdate -expand -group FPU0 $fpu0/i_fpu/out_ready_i
 add wave -noupdate -expand -group {FPU0 LSU} $fpu0/i_snitch_lsu/lsu_qready_o
@@ -21,6 +32,11 @@ add wave -noupdate -expand -group {FPU0 LSU} $fpu0/i_snitch_lsu/lsu_pready_i
 add wave -noupdate -expand -group {FPU0 LSU} $fpu0/i_snitch_lsu/lsu_pvalid_o
 
 add wave -noupdate -divider SSR
+# add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/cfg_word_i
+# add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/cfg_rdata_o
+# add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/cfg_wdata_i
+# add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/cfg_write_i
+
 add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/mem_addr_o
 add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/mem_write_o
 add wave -noupdate -expand -group {SSR0} $ssr0/i_addr_gen/mem_valid_o
@@ -35,8 +51,16 @@ add wave -noupdate -expand -group {SSR1} $ssr1/i_addr_gen/mem_ready_i
 add wave -noupdate -expand -group {SSR1} $ssr1/data_rsp
 add wave -noupdate -expand -group {SSR1} $ssr1/data_req
 
-# WaveRestoreCursors {{Cursor 1} {1548009 ps} 1} {{Cursor 2} {1559980 ps} 1} {{Cursor 3} {1560471 ps} 0}
-# quietly wave cursor active 3
+add wave -noupdate -divider {Perf Counters}
+set perf_counter $cluster0/i_snitch_cluster_peripheral
+add wave -noupdate -expand -group {PERF COUNTER} $perf_counter/reg2hw.perf_counter_enable
+add wave -noupdate -expand -group {PERF COUNTER} $perf_counter/reg2hw.perf_counter
+add wave -noupdate -expand -group {PERF COUNTER} $perf_counter/core_events_i
+add wave -noupdate -expand -group {PERF COUNTER} $perf_counter/tcdm_events_i
+
+
+WaveRestoreCursors {{Cursor 1} {10685 ns} 1} {{Cursor 2} {10273 ns} 1} {{Cursor 3} {1560471 ps} 0}
+quietly wave cursor active 3
 configure wave -namecolwidth 195
 configure wave -valuecolwidth 232
 configure wave -justifyvalue left
@@ -51,4 +75,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ps
 update
-WaveRestoreZoom {1544281 ps} {1585087 ps}
+# WaveRestoreZoom {1544281 ps} {1585087 ps}
