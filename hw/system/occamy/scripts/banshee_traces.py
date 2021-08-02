dbd4dbd79ee42b82c3b65886190348bd229bc9cc
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-02 15:37:08 +0200

    traces: format FP32 SIMD

--- hw/system/occamy/scripts/banshee_traces.py
+++ hw/system/occamy/scripts/banshee_traces.py
@@ -4,13 +4,23 @@ import struct
 
 
 def main():
-    fpu_ops = ['fmadd', 'fmul', 'fadd', 'fld', 'fsd', 'fsub.d', 'fsgnjx.d']
+    fpu_ops = ['fmadd', 'fmul', 'fadd', 'fld', 'fsd', 'fsub', 'fsgnjx', 'flt']
 
     for line in sys.stdin:
         if any(op in line for op in fpu_ops):
-            matches = re.finditer('([a-f0-9]{16})', line)
-            for i in matches:
-                line = re.sub(i.group(1), str(struct.unpack('!d', bytes.fromhex(i.group(1)))[0]), line)
+            float_hex = re.finditer('([a-f0-9]{16})', line)
+            for i in float_hex:
+                if '.s' in line:
+                    sub = f"{struct.unpack('!f', bytes.fromhex(i.group(1)[8:]))[0]:.5}"
+                else:
+                    sub = f"{struct.unpack('!d', bytes.fromhex(i.group(1)))[0]:.5}"
+                line = re.sub(i.group(1), sub, line)
+
+        if 'unknown' in line:
+            float_hex = re.finditer('(f[0-9]{1,2}[:=]{1})([a-f0-9]{16})', line)
+            for i in float_hex:
+                vec2, vec1 = (struct.unpack('!f', bytes.fromhex(i.group(2)[:8]))[0], struct.unpack('!f', bytes.fromhex(i.group(2)[8:]))[0])
+                line = re.sub(i.group(2), f'[{vec2:.4f}, {vec1:.4f}]', line)
         print(line, end='')
 
 
