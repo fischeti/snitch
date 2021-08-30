9313333da067415d7056fd30513dc40b3dd60429
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-08-30 16:59:01 +0200

    scripts: banshee_traces now formats fp16

--- hw/system/occamy/scripts/banshee_traces.py
+++ hw/system/occamy/scripts/banshee_traces.py
@@ -4,6 +4,7 @@ import struct
 import os
 from tqdm import tqdm
 
+
 def main():
     fpu_ops = ['fmadd', 'fmul', 'fadd', 'fld', 'fsd', 'fsub', 'fsgnjx', 'flt']
 
@@ -11,7 +12,7 @@ def main():
     file = f'{path}/../logs/banshee/traces.txt'
 
     with open(file, 'r') as traces:
-        num_lines = sum(1 for line in open(file,'r'))
+        num_lines = sum(1 for line in open(file, 'r'))
         for line in tqdm(traces, total=num_lines, ncols=100):
             hart = line.split()[2]
             if any(op in line for op in fpu_ops):
@@ -25,10 +26,15 @@ def main():
 
             if 'unknown' in line:
                 float_hex = re.finditer('(f[0-9]{1,2}[:=]{1})([a-f0-9]{16})', line)
-                for i in float_hex:
-                    vec2, vec1 = (struct.unpack('!f', bytes.fromhex(i.group(2)[:8]))[0], struct.unpack('!f', bytes.fromhex(i.group(2)[8:]))[0])
-                    line = re.sub(i.group(2), f'[{vec2:.4f}, {vec1:.4f}]', line)
-            trace_file = open(f'{path}/../logs/banshee//trace_hart_{hart}.txt', 'a')
+                if 'fp16' in sys.argv:
+                    for i in float_hex:
+                        vec = (struct.unpack('!e', bytes.fromhex(i.group(2)[:4]))[0], struct.unpack('!e', bytes.fromhex(i.group(2)[4:8]))[0], struct.unpack('!e', bytes.fromhex(i.group(2)[8:12]))[0], struct.unpack('!e', bytes.fromhex(i.group(2)[12:16]))[0])
+                        line = re.sub(i.group(2), f'[{vec[0]:.4f}, {vec[1]:.4f}, {vec[2]:.4f}, {vec[3]:.4f}]', line)
+                else:
+                    for i in float_hex:
+                        vec2, vec1 = (struct.unpack('!f', bytes.fromhex(i.group(2)[:8]))[0], struct.unpack('!f', bytes.fromhex(i.group(2)[8:]))[0])
+                        line = re.sub(i.group(2), f'[{vec2:.4f}, {vec1:.4f}]', line)
+            trace_file = open(f'{path}/../logs/banshee/trace_hart_{hart}.txt', 'a')
             trace_file.write(line)
             trace_file.close()
 
