3bae2230f5f2a7e49f0aaeee50427364642b2735
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-09-01 11:49:02 +0200

    net: Small fix

--- sw/applications/src/net_conv2d.c
+++ sw/applications/src/net_conv2d.c
@@ -10,13 +10,17 @@
 #include "math.h"
 #include "printf.h"
 
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
 int main() {
 
     conv_l.ifmap = (double*)ifmap_dram;
     conv_l.weights = (double*)weights0_dram;
     conv_l.ofmap = (double*)ofmap_dram;
-    conv_l.TILE_CI = 32;
+    conv_l.TILE_CI = min(32, conv_l.CI);
     conv_l.pad = (conv_l.FH-1) / 2;
+    conv_l.cluster2cluster = 0;
 
     conv2d_layer(conv_l);
 
