00c44e173fc59e51daabac57e3720104657ef18d
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-09-14 13:40:45 +0200

    Run on snitch_cluster

--- hw/system/occamy/scripts/banshee_traces.py
+++ hw/system/occamy/scripts/banshee_traces.py
@@ -1,20 +1,31 @@
-import sys
-import re
+#!/usr/bin/env python3
+
 import struct
-import os
+import re
 from tqdm import tqdm
+import argparse
+import pathlib
+
+FPU_OPS = ['fmadd', 'fmul', 'fadd', 'fld', 'fsd', 'fsub', 'fsgnjx', 'flt']
+
 
 def main():
-    fpu_ops = ['fmadd', 'fmul', 'fadd', 'fld', 'fsd', 'fsub', 'fsgnjx', 'flt']
 
-    path = os.path.dirname(os.path.realpath(__file__))
-    file = f'{path}/../logs/banshee/traces.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file',
+                        required=True,
+                        type=pathlib.Path)
 
-    with open(file, 'r') as traces:
-        num_lines = sum(1 for line in open(file,'r'))
+    args = parser.parse_args()
+
+    trace_file = args.file
+    cwd = args.file.parent.resolve()
+
+    with trace_file.open('r') as traces:
+        num_lines = sum(1 for line in open(trace_file,'r'))
         for line in tqdm(traces, total=num_lines, ncols=100):
             hart = line.split()[2]
-            if any(op in line for op in fpu_ops):
+            if any(op in line for op in FPU_OPS):
                 float_hex = re.finditer('([a-f0-9]{16})', line)
                 for i in float_hex:
                     if '.s' in line:
@@ -28,7 +39,7 @@ def main():
                 for i in float_hex:
                     vec2, vec1 = (struct.unpack('!f', bytes.fromhex(i.group(2)[:8]))[0], struct.unpack('!f', bytes.fromhex(i.group(2)[8:]))[0])
                     line = re.sub(i.group(2), f'[{vec2:.4f}, {vec1:.4f}]', line)
-            trace_file = open(f'{path}/../logs/banshee//trace_hart_{hart}.txt', 'a')
+            trace_file = open(f'{cwd}/trace_hart_{hart}.txt', 'a')
             trace_file.write(line)
             trace_file.close()
 
