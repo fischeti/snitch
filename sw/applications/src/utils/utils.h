7caf9d702f003633f7efb81cd34cd4609d4e916e
Tim Fischer <fischeti@iis.ee.ethz.ch>
2021-09-23 13:03:09 +0200

    Add DMA tracking to runtime

--- sw/applications/src/utils/utils.h
+++ sw/applications/src/utils/utils.h
@@ -2,6 +2,11 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
+#include "snrt.h"
+#include "layer.h"
+
 uint32_t benchmark_get_cycle();
-void check_layer(layer l, double* checksum);
+void snrt_dma_start_tracking();
+void snrt_dma_stop_tracking();
+uint32_t check_layer(layer l, double* checksum);
 void dma_memset(void *ptr, uint8_t value, uint32_t len);
